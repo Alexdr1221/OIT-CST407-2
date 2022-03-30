5fe73377dc4dd6554900bfc0ca47a01b1bf5b850
Alex Diaz Rodriguez <alexdr1221@gmail.com>
2022-03-30 13:57:15 -0700

    Added ability to display audio waveform

--- Testing/audio_read_test.py
+++ Testing/audio_read_test.py
@@ -1,17 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pyaudio
 import wave
 
-audio = pyaudio.PyAudio()
-rec = wave.open("testRecord.wav", "rb")
+def playback(file):
+    audio = pyaudio.PyAudio()
+    rec = wave.open(file, "rb")
 
-stream = audio.open(format=audio.get_format_from_width(rec.getsampwidth()),
-                    channels=rec.getnchannels(),
-                    rate=rec.getframerate(),
-                    output=True)
+    stream = audio.open(format=audio.get_format_from_width(rec.getsampwidth()),
+                        channels=rec.getnchannels(),
+                        rate=rec.getframerate(),
+                        output=True)
 
-data = rec.readframes(1024)
-
-print("Playing file...")
-while len(data) > 0:
-    stream.write(data)
     data = rec.readframes(1024)
+
+    print("Playing file...")
+    while len(data) > 0:
+        stream.write(data)
+        data = rec.readframes(1024)
+
+def waveform(file):
+    rec = wave.open(file, "rb")
+
+    signal = rec.readframes(-1)
+    signal = np.frombuffer(signal, dtype="int16")
+
+    f_rate = rec.getframerate()
+
+    time = np.linspace(0,
+                       len(signal) / f_rate,
+                       num = len(signal))
+
+    plt.figure(1)
+    plt.title("Sound Wave")
+    plt.ylabel("Amplitude(dB)")
+    plt.xlabel("Time(s)")
+    plt.plot(time, signal)
+    plt.show()
+
+
+######################################################################################
+
+file = "testRecord.wav"
+
+waveform(file)
+# playback(file)
