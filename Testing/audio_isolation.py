2b3162ae157deded2618cd147b0a6a6cac79ca92
Alex Diaz Rodriguez <alexdr1221@gmail.com>
2022-04-10 17:25:41 -0700

    Integrated support for stereo files for audio isolation method.
    Also fixed a bug where negative sample values were not compared against the threshold properly

--- Testing/audio_isolation.py
+++ Testing/audio_isolation.py
@@ -30,29 +30,74 @@ def trim_audio(file, start_threshold, end_threshold):
     sample_rate = 44100
     frame_size = 1024
 
-    # Open the input and output files
+    # Open the input file
     rec = wave.open(file, "rb")
-    dump = open("audio_dump.txt", "w")
 
-    # Read the signal data and convert it to an integer array
-    signal = rec.readframes(-1)
-    signal = np.frombuffer(signal, dtype="int16")
+    # Stereo Processing
+    if rec.getnchannels() == 2:
+        # Get the audio parameters
+        (nchannels, sampwidth, framerate, nframes, comptype, compname) = rec.getparams()
+        assert comptype == 'NONE'  # Compressed not supported yet
 
-    # Locate the first and last index where the signal value is above the desired threshold
-    firstval = min(idx for idx, val in enumerate(signal) if val > end_threshold)
-    secondval = max(idx for idx, val in enumerate(signal) if val > end_threshold)
+        # Read the signal data and convert it to an integer array
+        signal = rec.readframes(-1)
 
-    # Construct the trimmed array
-    trimmed = signal[firstval:secondval]
+        # Convert the signal array into raw sample values
+        signal = np.frombuffer(signal, dtype="int16")
 
-    # Save the audio as a .wav file
-    trimmed = np.int16(trimmed/np.max(np.abs(trimmed)) * 32767)
-    write("trimmedRecord.wav", 44100, trimmed)
+        # Split the signal into left and right channels
+        left_channel = signal[0::2]
+        right_channel = signal[1::2]
+
+        # Locate the first and last index for each channel where the signal value is above the desired threshold
+        left_first = min(idx for idx, val in enumerate(left_channel) if abs(val) > start_threshold)
+        left_second = max(idx for idx, val in enumerate(left_channel) if abs(val) > end_threshold)
+        right_first = min(idx for idx, val in enumerate(right_channel) if abs(val) > start_threshold)
+        right_second = max(idx for idx, val in enumerate(right_channel) if abs(val) > end_threshold)
+
+        # Determine the true start and end of the interesting part
+        firstval = left_first if left_first < right_first else right_first
+        secondval = left_second if left_second > right_second else right_second
+
+        # Construct the trimmed arrays
+        left_trim = left_channel[firstval:secondval]
+        right_trim = right_channel[firstval:secondval]
+
+        # Create the stereo array and populate
+        stereo = 2 * right_trim.tolist()
+        stereo[0::2] = left_trim
+        stereo[1::2] = right_trim
+        stereo = np.array(stereo)
+
+        # Create the stereo file, set the proper parameters, and write the data
+        ofile = wave.open("trimmedRecord.wav", 'w')
+        ofile.setparams((2, sampwidth, framerate, nframes, comptype, compname))
+        ofile.writeframes(stereo.tobytes())
+        ofile.close()
+        return
+
+    # Mono Processing
+    else:
+        # Read the signal data and convert it to an integer array
+        signal = rec.readframes(-1)
+        signal = np.frombuffer(signal, dtype="int16")
+
+        # Locate the first and last index where the signal value is above the desired threshold
+        firstval = min(idx for idx, val in enumerate(signal) if val > end_threshold)
+        secondval = max(idx for idx, val in enumerate(signal) if val > end_threshold)
+
+        # Construct the trimmed array
+        trimmed = signal[firstval:secondval]
+
+        # Save the audio as a .wav file
+        trimmed = np.int16(trimmed/np.max(np.abs(trimmed)) * 32767)
+        write("trimmedRecord.wav", 44100, trimmed)
 
 ###########################################################
 
-file = "testRecord.wav"
+file = "timeShifted.wav"
 trimmed = "trimmedRecord.wav"
 
 trim_audio(file, 1000, 2000)
 audio_read_test.waveform(trimmed)
+dump_audio(trimmed)
